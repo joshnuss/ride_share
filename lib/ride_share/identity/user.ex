c04c34dda4635f377e86a31f0751f99a1e9a922d
Josh Nussbaum <joshnuss@gmail.com>
2018-09-10 23:31:06 -0400

    Adds schema for User

---
+++ lib/ride_share/identity/user.ex
@@ -0,0 +1,20 @@
+defmodule RideShare.Identity.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :first_name, :last_name])
+    |> validate_required([:email, :first_name, :last_name])
+    |> unique_constraint(:email)
+  end
+end
---
+++ test/models/identity/user_test.exs
@@ -0,0 +1,47 @@
+defmodule RideShare.Identity.UserTest do
+  use RideShare.ModelCase
+
+  alias RideShare.Identity.User
+
+  @valid_attrs %{email: "user@example.com", first_name: "John", last_name: "Smith"}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset without email is invalid" do
+    changeset = User.changeset(%User{}, %{email: ""})
+
+    refute changeset.valid?
+    assert changeset.errors[:email]
+  end
+
+  test "changeset without first_name is invalid" do
+    changeset = User.changeset(%User{}, %{first_name: ""})
+
+    refute changeset.valid?
+    assert changeset.errors[:first_name]
+  end
+
+  test "changeset without last_name is invalid" do
+    changeset = User.changeset(%User{}, %{last_name: ""})
+
+    refute changeset.valid?
+    assert changeset.errors[:last_name]
+  end
+
+  test "changeset without duplicate email is invalid" do
+    insert_user(@valid_attrs)
+    {:error, changeset} = insert_user(@valid_attrs)
+
+    refute changeset.valid?
+    assert changeset.errors[:email]
+  end
+
+  defp insert_user(attributes) do
+    %User{}
+    |> User.changeset(@valid_attrs)
+    |> Repo.insert()
+  end
+end
