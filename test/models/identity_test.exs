efa703c5a86bef79b3d0a1408d201405469a3a82
Josh Nussbaum <joshnuss@gmail.com>
2018-09-12 16:39:37 -0400

    Beefs up register_user test

--- test/models/identity_test.exs
+++ test/models/identity_test.exs
@@ -3,14 +3,14 @@ defmodule RideShare.IdentityTest do
 
   alias RideShare.Identity
 
-  test "register/1 creates a user" do
-    {:ok, _} = Identity.register_user(%{
+  test "register_user/1 creates a user" do
+    {:ok, user} = Identity.register_user(%{
       email: "user@example.com",
       given_name: "John",
       family_name: "Smith"
     })
 
-    assert Identity.get_user_by(email: "user@example.com")
+    assert user == Identity.get_user_by(email: "user@example.com")
   end
 
   test "get_user_by/1 returns nil when it's missing" do
